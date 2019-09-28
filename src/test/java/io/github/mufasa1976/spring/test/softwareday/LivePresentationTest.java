7d1add062775b58f252d8a744a2a2d57ddf4dc85
mufasa1976 <mufasa1976@coolstuff.software>
2019-09-28 14:58:06 +0200

    switched to JUnit 5

--- src/test/java/io/github/mufasa1976/spring/test/softwareday/LivePresentationTest.java
+++ src/test/java/io/github/mufasa1976/spring/test/softwareday/LivePresentationTest.java
@@ -1,41 +1,44 @@
 package io.github.mufasa1976.spring.test.softwareday;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.fail;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class LivePresentationTest {
+@DisplayName("Live Demo for the Presentation")
+class LivePresentationTest {
   @Autowired
   private MockMvc restInterface;
 
   @Test
-  public void findPerson_OK_byId_1() throws Exception {
+  @DisplayName("Find the Person with ID 1 and expect its Name is Erich Stadler")
+  void findPerson_OK_byId_1() throws Exception {
     fail("not implemented now");
   }
 
   @Test
-  public void findPerson_NOK_unknownId() throws Exception {
+  @DisplayName("Find the Person with ID 9999999999 and expect it hasn't been found")
+  void findPerson_NOK_unknownId() throws Exception {
     fail("not implemented now");
   }
 
   @Test
-  public void findAllPersons_OK() throws Exception {
+  @DisplayName("Find all Persons and expect 3 have been found")
+  void findAllPersons_OK() throws Exception {
     fail("not implemented now");
   }
 
   @Test
+  @DisplayName("remove all Persons from the Database and expect no Persons have been found")
   @Transactional
-  public void findAllPersons_OK_allPersonsRemovedBeforeTest() throws Exception {
+  void findAllPersons_OK_allPersonsRemovedBeforeTest() throws Exception {
     fail("not implemented now. Remember: autowire EntityManager to run this Test");
   }
 }
